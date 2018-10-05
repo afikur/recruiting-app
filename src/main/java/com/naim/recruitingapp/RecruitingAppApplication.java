0bfcbed54631c72209f5c22bbcc87e4c95b98ca3
Afikur Rahman <this.afikur@gmail.com>
2018-10-06 02:04:52 +0600

    setup project

---
+++ src/main/java/com/naim/recruitingapp/RecruitingAppApplication.java
@@ -0,0 +1,12 @@
+package com.naim.recruitingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecruitingAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecruitingAppApplication.class, args);
+    }
+}
